09d024e348b49c7536af23f67d48fa9ab58f618d
boehkarl <146597696+boehkarl@users.noreply.github.com>
2023-12-10 17:56:25 -0600

    Update splunk_init.sh

--- splunk_init.sh
+++ splunk_init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Setting stuff up"
 echo "Grabbing scripts"
-curl "https://raw.githubusercontent.com/boehkarl/CentOS6/main/repos_test" >> repos.sh
+curl "https://raw.githubusercontent.com/boehkarl/CentOS6/main/repos.sh" >> repos.sh
 curl "https://raw.githubusercontent.com/boehkarl/CentOS6/main/firewall.sh" >> firewall.sh
 curl "https://raw.githubusercontent.com/boehkarl/CentOS6/main/sudowoodo.sh" >> sudowoodo.sh
 chmod 755 repos.sh firewall.sh sudowoodo.sh

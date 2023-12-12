1c6b00255cbfdea5d024a6570600d449e982ddfc
boehkarl <146597696+boehkarl@users.noreply.github.com>
2023-12-11 18:31:05 -0600

    Update splunk_firewall.sh

--- splunk_firewall.sh
+++ splunk_firewall.sh
@@ -8,23 +8,23 @@ iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
 # HTTP & HTTPS rules 
-iptables -A INPUT -p tcp --dport 80 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 80 -m state --state ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp --dport 443 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 443 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --sport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --sport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
 
 # Splunk WebGUI rules 
-iptables -A INPUT -p tcp --dport 8000 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 8000 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --dport 8000 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 8000 -j ACCEPT
 
 # Splunk Management Port
-iptables -A INPUT -p tcp --dport 8089 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --dport 8089 -j ACCEPT
 
 # Syslog traffic
-iptables -A INPUT -p tcp --dport 9997 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp --dport 9998 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p tcp --dport 601 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p udp --dport 514 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --dport 9997 -j ACCEPT
+iptables -A INPUT -p tcp --dport 9998 -j ACCEPT
+iptables -A INPUT -p tcp --dport 601 -j ACCEPT
+iptables -A INPUT -p udp --dport 514 -j ACCEPT
 
 # Kill SSH traffic
 iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j DROP

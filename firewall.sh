ca9f7b36515b27b75b8e91af1db547f9c91e2918
boehkarl <146597696+boehkarl@users.noreply.github.com>
2023-12-12 18:07:20 -0600

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -154,10 +154,6 @@ setSplunk(){
   iptables -A INPUT -p tcp --dport 601 -j ACCEPT
   iptables -A INPUT -p udp --dport 514 -j ACCEPT
 
-  # Kill SSH traffic
-  iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j DROP
-  iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j DROP
-
   #allowSysLog   # Opens the required ports for syslogs to be forwarded to datalake
   dropAll
   showFirewall

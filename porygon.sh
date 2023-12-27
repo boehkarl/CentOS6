1ebfc5fb71d23bdbb8ad3bea40b2a603cb4d23af
boehkarl <146597696+boehkarl@users.noreply.github.com>
2023-12-26 18:46:30 -0600

    Update porygon.sh

--- porygon.sh
+++ porygon.sh
@@ -1,1 +1,7 @@
 #Script to automate backups and recovery
+#Ideas:
+#Backup /etc /var /bin separate from /opt which contains splunk
+#Make options for if I want to restore /etc /var /bin  OR  /opt
+
+#One option for regular tar backup
+#One option for compressed backup

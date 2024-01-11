74aab75ab406a22f1f4c2acc605681450ebc4771
boehkarl <146597696+boehkarl@users.noreply.github.com>
2024-01-10 19:11:03 -0600

    Update porygon.sh

--- porygon.sh
+++ porygon.sh
@@ -1,11 +1,4 @@
-echo "[upg]" >> /etc/yum.repos.d/upgradetool.repo
-echo "name=CentOS-$releasever – Upgrade Tool" >> /etc/yum.repos.d/upgradetool.repo
-echo "baseurl=http://dev.centos.org/centos/6/upg/x86_64/" >> /etc/yum.repos.d/upgradetool.repo
-echo "gpgcheck=1" >> /etc/yum.repos.d/upgradetool.repo
-echo "enabled=1" >> /etc/yum.repos.d/upgradetool.repo
-echo "gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6" >> /etc/yum.repos.d/upgradetool.repo
 
-yum install redhat-upgrade-tool preupgrade-assistant-contents
 
 
 
